172a377f76b57760bc2fa1c4063779a75be5fc28
Az107 <albertzruiz1@gmail.com>
2021-02-02 01:22:54 +0100

    test: :fire: Changed test framework
    The test framework was changed from jasmine to jest

---
+++ test/telomer.test.js
@@ -0,0 +1,9 @@
+var telomer = require("../build/index");
+var t = new telomer.telomer();
+
+var data = [1,2,3,4,5];
+
+
+test("add telomer to array", () => {
+    expect([1,1,2,3,4,5]).toEqual(t.addTelomer(data));
+})
